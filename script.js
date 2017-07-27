59dc868c66d858a4af2205e76d30cd3212cd7001
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-07-27 17:00:59 +0200

    update

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ function Phone(brand, price, color, size) {
 	this.price = price;
 	this.color = color;
   this.size = size;
-  valueOf = function getWarranyCost(price) {
-    return price * 0.1;
-  };
 }   
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", size is " + this.size + " and the price is " + this.price  + " and warrany cost " + valueOf(this.price) );
+  var getWarranyCost = function(price) {
+    return price * 0.1;
+  };
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", size is " + this.size + " and the price is " + this.price  + " and warrany cost " + getWarranyCost(this.price) );
 };
 var iPhone6S = new Phone("Apple", 2250, "silver", "small");
 var Lumia640 = new Phone ("Microsoft", 600, "black", "medium");
 var LG_K10 = new Phone ("LG", 2800, "red", "large");
-
 iPhone6S.printInfo();  
-Lumia640.printInfo();
+Lumia640.printInfo(); 
 LG_K10.printInfo();
